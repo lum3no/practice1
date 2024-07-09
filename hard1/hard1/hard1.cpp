2d96db67f2f803ed36f1f4d2d95084863b09a17b
lum3no <fima.afisha@mail.ru>
2024-07-09 21:17:35 +0300

    1 hard

---
+++ hard1/hard1/hard1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int countOnes(int n) {
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        int j = i;
+        while (j > 0) {
+            if (j % 10 == 1) {
+                count++;
+            }
+            j /= 10;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "Введите число n: ";
+    cin >> n;
+    cout << "Общее количество цифр 1 во всех неотрицательных целых числах, меньших или равных " << n << ", равно: " << countOnes(n) << endl;
+    return 0;
+}
